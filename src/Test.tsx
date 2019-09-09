df78f0910335de5cbdd8883168295818371de5ed
Marcus Ekeberg <marcuseg@axis.com>
2019-09-09 12:11:00 +0200

    Fixed reading from all pages. Have to fix bug with no space between name and value

--- src/Test.tsx
+++ src/Test.tsx
@@ -1,10 +1,6 @@
 import * as React from 'react'
+import * as data from './fonderMock.json';
 
-const content = [
-    "<div class=\"root\"><span class=\"toprow\"> 238 SVT Text         Söndag 08 sep 2019\n </span><span class=\"W\">Källa: SIX          190905         1(3)</span>\n <span class=\"Y bgY DH\"> </span><span class=\"Y bgY DH\"> </span><span class=\"Y bgY DH\"> </span><span class=\"B bgY DH\">PPM AKTIEFONDER  A-B      </span><span class=\"R bgY DH\">SLUTKURSER</span>\n <span class=\"W\">            Va-   Fond-   Utv. sek i % </span>\n <span class=\"W\">            luta  kurs    1 dag    i år</span>\n <span class=\"G\">                                       </span>\n <span class=\"G\">Aberdeen Asset Management              </span>\n <span class=\"W\">American EquUSD    32.84 </span><span class=\"Y\">  0.36 </span><span class=\"Y\">  31.93</span>\n <span class=\"W\">- Japanese  JPY          </span><span class=\"W\">       </span><span class=\"W\">       </span>\n <span class=\"W\">- Japanese  JPY  1530.15 </span><span class=\"C\"> -0.48 </span><span class=\"Y\">  22.06</span>\n <span class=\"W\">                                       </span>\n <span class=\"G\">Aktie-Ansvar                           </span>\n <span class=\"W\">Europa            130.22 </span><span class=\"C\"> -0.78 </span><span class=\"Y\">  14.91</span>\n <span class=\"W\">Sverige A         584.14 </span><span class=\"C\"> -0.17 </span><span class=\"Y\">   9.96</span>\n <span class=\"W\">                                       </span>\n <span class=\"G\">Aktiespararna                          </span>\n <span class=\"W\">Direktavkastning  134.88 </span><span class=\"W\">       </span><span class=\"Y\">  18.93</span>\n <span class=\"W\">Topp Sv.           17.37 </span><span class=\"W\">       </span><span class=\"Y\">  16.89</span>\n <span class=\"W\">                                       </span>\n <span class=\"G\">Alfred Berg Kapitalforvaltning         </span>\n <span class=\"W\">Aktiv       NOK  1610.71 </span><span class=\"C\"> -0.40 </span><span class=\"Y\">  14.19</span>\n <span class=\"W\">BestSelect  NOK          </span><span class=\"W\">       </span><span class=\"W\">       </span>\n <span class='added-line'>                                       </span>\n <span class=\"W\">Fastighet Nord.   <a href=\"/489\">489</a>.85 </span><span class=\"Y\">  0.14 </span><span class=\"Y\">  40.15</span>\n</div>",
-    "<div class=\"root sub\"><span class=\"toprow\"> 238 SVT Text         Söndag 08 sep 2019\n </span><span class=\"W\">Källa: SIX          190905         2(3)</span>\n <span class=\"Y bgY DH\"> </span><span class=\"Y bgY DH\"> </span><span class=\"Y bgY DH\"> </span><span class=\"B bgY DH\">PPM AKTIEFONDER  A-B      </span><span class=\"R bgY DH\">SLUTKURSER</span>\n <span class=\"W\">            Va-   Fond-   Utv. sek i % </span>\n <span class=\"W\">            luta  kurs    1 dag    i år</span>\n <span class=\"G\">                                       </span>\n <span class=\"G\">Alfred Berg Kapitalforvaltning         </span>\n <span class=\"W\">Gambak      NOK 32010.02 </span><span class=\"C\"> -0.40 </span><span class=\"Y\">  18.43</span>\n <span class=\"W\">Berg Global NOK   542.06 </span><span class=\"W\">       </span><span class=\"Y\">  29.42</span>\n <span class=\"W\">Akt.Norge   NOK   991.38 </span><span class=\"C\"> -0.40 </span><span class=\"Y\">  15.23</span>\n <span class=\"W\">Ryssland          625.20 </span><span class=\"C\"> -0.30 </span><span class=\"Y\">  31.69</span>\n <span class=\"W\">                                       </span>\n <span class=\"G\">Allianz Global Investors               </span>\n <span class=\"W\">Europe Equi EUR   271.00 </span><span class=\"C\"> -0.69 </span><span class=\"Y\">  28.29</span>\n <span class=\"W\">                                       </span>\n <span class=\"G\">AMF Fonder                             </span>\n <span class=\"W\">Asien SH          221.27 </span><span class=\"C\"> -0.28 </span><span class=\"Y\">  17.62</span>\n <span class=\"W\">Europa            214.30 </span><span class=\"C\"> -0.76 </span><span class=\"Y\">  20.54</span>\n <span class=\"W\">Global            196.96 </span><span class=\"C\"> -0.54 </span><span class=\"Y\">  25.75</span>\n <span class=\"W\">Nordamerika       297.27 </span><span class=\"C\"> -0.48 </span><span class=\"Y\">  31.21</span>\n <span class=\"W\">Småbolag          720.62 </span><span class=\"C\"> -0.04 </span><span class=\"Y\">  23.87</span>\n <span class=\"W\">Sverige           604.39 </span><span class=\"Y\">  0.04 </span><span class=\"Y\">  15.03</span>\n <span class='added-line'>                                       </span>\n <span class=\"W\">Världen           <a href=\"/417\">417</a>.13 </span><span class=\"C\"> -0.25 </span><span class=\"Y\">  18.90</span>\n</div>",
-    "<div class=\"root sub\"><span class=\"toprow\"> 238 SVT Text         Söndag 08 sep 2019\n </span><span class=\"W\">Källa: SIX          190905         3(3)</span>\n <span class=\"Y bgY DH\"> </span><span class=\"Y bgY DH\"> </span><span class=\"Y bgY DH\"> </span><span class=\"B bgY DH\">PPM AKTIEFONDER  A-B      </span><span class=\"R bgY DH\">SLUTKURSER</span>\n <span class=\"W\">            Va-   Fond-   Utv. sek i % </span>\n <span class=\"W\">            luta  kurs    1 dag    i år</span>\n <span class=\"G\">                                       </span>\n <span class=\"G\">Amundi Funds                           </span>\n <span class=\"W\">Glb.GoldMineUSD    42.51 </span><span class=\"C\"> -0.73 </span><span class=\"Y\">  52.85</span>\n <span class=\"W\">                                       </span>\n <span class=\"G\">Avanza Fonder                          </span>\n <span class=\"W\">ZERO              230.81 </span><span class=\"Y\">  0.28 </span><span class=\"Y\">  17.53</span>\n <span class=\"W\">                                       </span>\n <span class=\"G\">Banque de Luxembourg                   </span>\n <span class=\"W\">America     USD  8602.01 </span><span class=\"C\"> -0.73 </span><span class=\"Y\">  37.24</span>\n <span class=\"W\">Europe      EUR  8006.08 </span><span class=\"C\"> -0.69 </span><span class=\"Y\">  25.79</span>\n <span class=\"W\">Global Eq   EUR   944.73 </span><span class=\"C\"> -0.69 </span><span class=\"Y\">  25.57</span>\n <span class=\"W\">Horizon     EUR  1252.86 </span><span class=\"C\"> -0.69 </span><span class=\"Y\">  25.88</span>\n <span class=\"W\">                                       </span>\n <span class=\"G\">BlackRock                              </span>\n <span class=\"W\">EmergEurop  EUR   110.94 </span><span class=\"C\"> -0.44 </span><span class=\"Y\">  27.25</span>\n <span class=\"W\">Emerg Mkts  USD    36.10 </span><span class=\"C\"> -0.37 </span><span class=\"Y\">  20.87</span>\n <span class=\"W\">EuroMarket  EUR    29.22 </span><span class=\"C\"> -0.42 </span><span class=\"Y\">  25.54</span>\n <span class='added-line'>                                       </span>\n <span class=\"W\">European Va EUR    61.51 </span><span class=\"C\"> -0.66 </span><span class=\"Y\">  16.17</span>\n</div>"
-]
 interface IFundInfo {
     company: string | null
     funds: IFundDetail[];
@@ -12,9 +8,9 @@ interface IFundInfo {
 
 interface IFundDetail {
     name: string;
-    currentValue: string
-    dailyPercentage: string
-    yearlyPercentage: string
+    currentValue: number
+    dailyPercentage: number
+    yearlyPercentage: number
 }
 
 class FundInfo implements IFundInfo {
@@ -28,16 +24,16 @@ class FundInfo implements IFundInfo {
 
 class FundDetail implements IFundDetail {
     public name: string;
-    public currentValue: string
-    public dailyPercentage: string
-    public yearlyPercentage: string
+    public currentValue: number
+    public dailyPercentage: number
+    public yearlyPercentage: number
 
-    constructor(name: string, currentValue: string) {
+    constructor(name: string, currentValue: number) {
         this.name = name
         this.currentValue = currentValue
-        this.dailyPercentage = ''
-        this.yearlyPercentage = ''
-        
+        this.dailyPercentage = 0
+        this.yearlyPercentage = 0
+
     }
 }
 
@@ -49,33 +45,47 @@ export class Test extends React.Component {
         super(props);
         const document = new Document();
         const topElement: HTMLElement = document.createElement('rootEl');
-        topElement.insertAdjacentHTML("afterbegin",content[0]);
+
+        const jsonData = JSON.stringify(data)
+        const fundData = JSON.parse(jsonData)
+        const pages = fundData.default;
+        const allContent = []
+        for (let index = 0; index < pages.length; index++) {
+            allContent.push(pages[index].content);
+        }
+
+        for (let index = 0; index < allContent.length; index++) {
+            const element = allContent[index];
+            topElement.insertAdjacentHTML("afterbegin",element);
+        }
         document.appendChild(topElement)
 
-        const root = document.getElementsByClassName("root")[0]
+        const rootElements = document.getElementsByClassName("root")
 
-        console.log(document)
 
-        let fundInfo = new FundInfo('');
-        for (let index = 0; index < root.childElementCount; index++) {
-            const element = root.children[index];
-            if (element.className === "G" && element.innerHTML.trim() !== '') {
-                if (fundInfo.funds.length > 0) {
-                    this.allFunds.push(fundInfo)
+        for (let index = 0; index < rootElements.length; index++) {
+            const root = rootElements[index];
+
+            let fundInfo = new FundInfo('');
+            for (let j = 0; j < root.childElementCount; j++) {
+                const element = root.children[j];
+                if (element.className === "G" && element.innerHTML.trim() !== '') {
+                    if (fundInfo.funds.length > 0) {
+                        this.allFunds.push(fundInfo)
+                    }
+                    fundInfo = new FundInfo(element.innerHTML.trim())
                 }
-                fundInfo = new FundInfo(element.innerHTML.trim())
-            }
-            if (fundInfo.company && element.className === "W" && element.innerHTML.trim() !== '') {
-                const fundDetail = this.getFundDetails([root.children[index], root.children[index+1],root.children[index+2]])
-                if (fundDetail) {
-                    fundInfo.funds.push(fundDetail);
+                if (fundInfo.company && element.className === "W" && element.innerHTML.trim() !== '') {
+                    const fundDetail = this.getFundDetails([root.children[j], root.children[j+1],root.children[j+2]])
+                    if (fundDetail) {
+                        fundInfo.funds.push(fundDetail);
+                    }
                 }
             }
+
         }
-        
+
         console.log(this.allFunds)
-
-
     }
 
     public render() {
@@ -87,14 +97,14 @@ export class Test extends React.Component {
     private getFundDetails(elements: Element[]): IFundDetail | null {
         const fundHeading = this.getFundNameAndValue(elements[0].innerHTML.trim());
         const fund = new FundDetail(fundHeading.name!, fundHeading.currentValue!);
-        
+
         for (let index = elements.length -1 ; index > 0; index--) {
             const element = elements[index];
             if (element.className === "C" || element.className === "Y") {
                 if (fund.yearlyPercentage) {
-                    fund.dailyPercentage = element.innerHTML.trim()    
+                    fund.dailyPercentage = Number(element.innerHTML.trim())
                 } else {
-                    fund.yearlyPercentage = element.innerHTML.trim()
+                    fund.yearlyPercentage = Number(element.innerHTML.trim())
                 }
              }
         }
@@ -104,13 +114,13 @@ export class Test extends React.Component {
     private getFundNameAndValue (heading: string): Partial<IFundDetail> {
         let readChar = 'X';
         let pos = heading.length
-        while (readChar !== ' ') {
+        while (readChar !== ' ' && pos !== 0) {
             readChar = heading.charAt(pos);
-            pos-- 
-        } 
+            pos--
+        }
         return {
             name: heading.slice(0,pos).trim(),
-            currentValue: heading.slice(pos+2, heading.length)
+            currentValue: Number(heading.slice(pos+2, heading.length))
         }
     }
 

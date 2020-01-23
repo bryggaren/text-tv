48a094a68d9c4eb4e2fb09e60a60a0ffafdd4b2c
Marcus Ekeberg <marcuseg@axis.com>
2020-01-23 16:12:07 +0100

    Updates

---
+++ precache-manifest.9405e120bba8c6a306fd7cd7e81fe741.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8118454b71859804015514852e6aba30",
+    "url": "/text-tv/index.html"
+  },
+  {
+    "revision": "5a3a71623bba1d7a1708",
+    "url": "/text-tv/static/css/main.16d98de6.chunk.css"
+  },
+  {
+    "revision": "ccb0f6a72678dfc924be",
+    "url": "/text-tv/static/js/2.5b7700e1.chunk.js"
+  },
+  {
+    "revision": "3a2edbac0b625403cc8cad9dc7e17c15",
+    "url": "/text-tv/static/js/2.5b7700e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a3a71623bba1d7a1708",
+    "url": "/text-tv/static/js/main.326a4b1b.chunk.js"
+  },
+  {
+    "revision": "0a1d5e997df75fb35359",
+    "url": "/text-tv/static/js/runtime-main.09f1be8c.js"
+  }
+]);

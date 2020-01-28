2c346ac80c5e4cf00fecf226bb2d35f732a7c36c
Marcus Ekeberg <marcuseg@axis.com>
2020-01-28 09:27:32 +0100

    Changed to https

--- src/communicators/TextTv.communicator.ts
+++ src/communicators/TextTv.communicator.ts
@@ -2,7 +2,7 @@ import * as React from 'react';
 class TextTvCommunicator {
     public async getFundPages(): Promise<any> {
         try {
-            const response = await fetch('http://api.texttv.nu/api/get/238-244?app=texttvnu.web');
+            const response = await fetch('https://api.texttv.nu/api/get/238-244?app=texttvnu.web');
             return response.ok
                 ? response.json()
                 : Promise.reject(
